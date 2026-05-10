d7bee1f49271287647a8ee22eddfc766055c1d85
ehi3 <successiy007@gmail.com>
2026-05-10 02:22:02 +0100

    fix: downgrade to Soda 3.x for stable Python API

--- ci/run_soda_scan.py
+++ ci/run_soda_scan.py
@@ -1,22 +1,22 @@
 """
 ci/run_soda_scan.py
-Runs a Soda Core 4.x contract scan.
+Runs a Soda Core 3.x contract scan.
 Usage: python3 ci/run_soda_scan.py <data_source> <config_path> <contract_path> <scan_name>
 """
 import sys
 from soda.scan import Scan
-
+ 
 data_source   = sys.argv[1]
 config_path   = sys.argv[2]
 contract_path = sys.argv[3]
 scan_name     = sys.argv[4]
-
+ 
 scan = Scan()
 scan.set_data_source_name(data_source)
 scan.add_configuration_yaml_file(config_path)
 scan.add_sodacl_yaml_file(contract_path)
 scan.set_scan_definition_name(scan_name)
-
+ 
 exit_code = scan.execute()
 print(scan.get_logs_text())
 sys.exit(exit_code)
