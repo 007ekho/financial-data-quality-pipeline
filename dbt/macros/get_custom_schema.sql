d076ea91037c24bc5d5e67a8b54a8b3018db1cd7
ehi3 <successiy007@gmail.com>
2026-05-11 01:30:49 +0100

    fix: unconditional schema override regardless of target name

--- dbt/macros/get_custom_schema.sql
+++ dbt/macros/get_custom_schema.sql
@@ -1,18 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-
-    {%- if target.name in ('prod', 'production', 'Production') and custom_schema_name is not none -%}
+    {%- if custom_schema_name is not none -%}
 
         {{ custom_schema_name | trim }}
 
-    {%- elif custom_schema_name is none -%}
-
-        {{ default_schema }}
-
     {%- else -%}
 
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ target.schema }}
 
     {%- endif -%}
 
